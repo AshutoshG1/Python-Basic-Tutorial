59d2ed9a4ca84d819652fba98498c7ff25dba601
AshutoshG1 <ashutoshgautamspn@gmail.com>
2025-01-25 20:49:09 +0530

    committed all

---
+++ FirstProgram.py
@@ -0,0 +1,74 @@
+print("Ashutosh is my name")
+print("Hello")
+print("Ashutosh is my name.","Hello World.")
+print(20)
+print(23+13)
+
+name = "Ashutosh"
+age = 20
+price = 20.99
+print(name, age, price)
+print("My name is : ",name)
+print("My age is : ",age)
+
+age2 = age
+print(age2)
+
+print(type(name))
+print(type(age))
+print(type(price))
+
+name1 = "Techie Ashutosh"
+name2 = 'Techie Ashutosh'
+name3 = '''Techie Ashutosh'''
+print(name1)
+print(name2)
+print(name3)
+
+a = False
+b = None
+c = True
+print(type(a))
+print(type(b))
+print(type(c))
+
+a = 2
+b = 5
+sum = a + b
+print(sum)
+sum = a - b
+print(sum)
+sum = a * b
+print(sum)
+sum = a / b
+print(sum)
+
+#Single line comment
+print("Single line comment")
+
+"""
+Multi-line comment
+This is a multi-line comment
+"""
+print("Multi-line comment")
+
+# name4 = input("Name : ")
+# print(name4)
+
+# age3 = int(input("Age : "))
+# print(age3)
+# price2 = float(input("Price : "))
+# print(price2)
+
+# light = input("light color : ")
+# if(light=="red"):
+#     print("stop")
+# elif(light=="yellow"):
+#     print("look")
+# else:
+#     print("go")
+
+#Ternary operator 
+food = input("food : ")
+eat = "yes" if food == "cake" else "no"
+print(eat)
