95ddadf327bad085c6c792fa6543cd8ee9f32bf7
AshutoshG1 <ashutoshgautamspn@gmail.com>
2025-01-27 01:43:22 +0530

    add new files

---
+++ SeventhProgram.py
@@ -0,0 +1,33 @@
+# f = open("demo.txt","r")
+# data = f.read()
+# print(data)
+# print(type(data))
+# f.close()
+
+# f = open("demo.txt","r")
+# # data = f.read(5)
+# # print(data)
+# # print(type(data))
+# line1 = f.readline()
+# print(line1)
+# line2 = f.readline()
+# print(line2)
+# f.close()
+
+# f = open("demo.txt","w")
+# f.write(" i want to java script also ")
+# f.close()
+
+# f = open("demo.txt","a")
+# f.write(" this is something appended ")
+# f.close()
+
+# with open("demo.txt","r") as f:
+#     data = f.read()
+#     print(data)
+
+# with open("demo.txt","w") as f:
+#     f.write("new data")
+
+import os
+os.remove(demo.txt)
---
+++ SixthProgram.py
@@ -0,0 +1,49 @@
+# #function definition 
+# def calc_sum(a,b): #parameter
+#     sum = a + b
+#     print(sum)
+#     return sum
+
+# sum = calc_sum(5,10) #arguments
+# print(sum)
+
+# def print_Hello():
+#     print("Hello")
+
+# print_Hello()
+# output = print_Hello()
+# print(output)
+
+# def calc_avg(a, b, c):
+#     sum = a + b + c
+#     avg = sum/3
+#     return avg
+
+# avg = calc_avg(5,10,15)
+# print(avg)
+
+# print("Techie",end=" ")
+# print("Ashutosh")
+
+# def cal_prod(a=1,b=2):
+#     print(a+b)
+#     return a+b
+
+# cal_prod()
+
+# #recursive function
+# def show(n):
+#     if(n==0):
+#         return
+#     print(n)
+#     show(n-1)
+
+# show(5)
+
+# def fact(n):
+#     if(n==0 or n==1):
+#         return 1
+#     return fact(n-1)*n
+
+# print(fact(5))
+
